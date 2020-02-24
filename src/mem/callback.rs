42f18ac292b7e7b1e9b49f7dec15ce8677e93011
Duknam Yoo <yoo2001818@gmail.com>
2020-02-24 23:57:59 +0900

    Add callback memory test

--- src/mem/callback.rs
+++ src/mem/callback.rs
@@ -22,3 +22,29 @@ impl Memory for CallbackMemory<'_> {
     (self.write_callback)(address, value)
   }
 }
+
+#[cfg(test)]
+mod tests {
+  use crate::mem::*;
+  use super::*;
+  use std::cell::RefCell;
+
+  #[test]
+  fn test() {
+    let last_addr = RefCell::new(0);
+    let last_value = RefCell::new(0);
+    let get_value = |addr: usize| -> u32 { addr as u32 };
+    let mut set_value = |addr: usize, value: u32| -> () {
+      *last_addr.borrow_mut() = addr;
+      *last_value.borrow_mut() = value;
+    };
+    let mut mem = CallbackMemory::new(
+      &get_value,
+      &mut set_value,
+    );
+    mem.write(1000, 0x7F7F7F7F);
+    assert_eq!(*last_addr.borrow(), 1000);
+    assert_eq!(*last_value.borrow(), 0x7F7F7F7F);
+    assert_eq!(mem.read(1000), 1000);
+  }
+}
