6561f9142a2d223139924c11557da510237f8821
Duknam Yoo <yoo2001818@gmail.com>
2020-03-25 23:57:23 +0900

    Add call test

--- tests/call.asm
+++ tests/call.asm
@@ -1,3 +1,17 @@
 ; Test set 0x0500
 call_test_1:
 ; We should test call, ret, int
+mov ax, 0x1000
+call call_test_1_func
+cmp ax, 0x1001
+assert e, 0x0500
+mov cx, call_test_1_func
+call cx
+cmp ax, 0x1002
+assert e, 0x0501
+jmp call_test_2
+call_test_1_func:
+inc ax
+ret
+call_test_2:
+; Do nothing
